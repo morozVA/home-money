61dafc08138bf8e822aaeb40b93d9f0246c9994a
morozVA <cadaver@tut.by>
2018-05-14 21:35:21 +0300

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
+      HttpModule,
+      AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
