30e840287c431bcb76199f66db058da8da256ec9
morozVA <cadaver@tut.by>
2018-06-23 14:57:32 +0300

    stsrt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { AppRoutingModule } from './app-routing.module';
-import { UsersService } from './shared/services/users.service';
-import { AuthService } from './shared/services/auth.service';
-import { AuthGuard } from './shared/services/auth.guard';
-import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    AuthModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [UsersService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
