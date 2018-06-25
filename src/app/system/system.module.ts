469cb846024084116bb6aaf0d8d6eb980674afa0
morozVA <cadaver@tut.by>
2018-06-25 22:39:19 +0300

    13

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -7,6 +7,7 @@ import { HistoryPageComponent } from './history-page/history-page.component';
 import { PlanningPageComponent } from './planning-page/planning-page.component';
 import { RecordsPageComponent } from './records-page/records-page.component';
 import {SystemComponent} from "./system.component";
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, SystemRoutingModule],
@@ -16,6 +17,7 @@ import {SystemComponent} from "./system.component";
     HistoryPageComponent,
     PlanningPageComponent,
     RecordsPageComponent,
+    SidebarComponent,
   ]
 })
 
