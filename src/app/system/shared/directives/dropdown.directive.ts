09c7b36e41b40d86c16dc4cdafb2507f67163980
morozVA <cadaver@tut.by>
2018-06-26 20:45:55 +0300

    14

---
+++ src/app/system/shared/directives/dropdown.directive.ts
@@ -0,0 +1,16 @@
+import {Directive, HostBinding, HostListener} from "@angular/core";
+
+@Directive({
+  selector: '[wfmDropdown]'
+})
+
+export class DropdownDirective {
+
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') onClick() {
+    this.isOpen = !this.isOpen;
+  }
+
+}
+
